62db2037cad9b220fe850bdac6a99e2733bcb2d6
diana-rahma <dianarhm12@gmail.com>
2023-10-15 13:41:21 +0700

    revisi login dgn looping

--- Login.java
+++ Login.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 public class Login {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int pin=12345678;
+        int Now=12345678;
         System.out.println("Masukkan PIN ATM anda: ");
-        pin = input.nextInt();
+        Now = input.nextInt();
 
-        if (pin==12345678)
-            System.out.println("Login berhasil");
-        else 
-            System.out.println("PIN anda salah, silahkan coba lagi");
+        do {
+            System.out.println("Masukkan PIN anda : ");
+            Now = input.nextInt();
+            if(Now == 12345678) {
+                continue;
+            }
+        } while (Now != 12345678);
     }
 }
