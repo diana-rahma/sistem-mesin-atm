29c27e8907ca99d8c6ff1c4070ede82faf30ea05
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2023-11-01 09:43:58 +0700

    proggress array poyek

--- RegistrasiArray.java
+++ RegistrasiArray.java
@@ -4,31 +4,23 @@ public class RegistrasiArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Masukkan jumlah Pendaftar : ");
-        int Pendaftar = sc.nextInt();
-        sc.nextLine(); 
+        String Nama, Asal, noHP;
+        int PIN;
 
-        String[] namaPend = new String[Pendaftar];
+        String [][] Identitas = new String [5][4];
 
-        for (int i = 0; i < Pendaftar; i++) {
-            System.out.print("Masukkan Pendaftar ke-" + (i+1) + ": ");
-            namaPend[i] = sc.nextLine();
-        }
+    System.out.println("Masukkan Nama Anda : ");
+    Nama = sc.nextLine();
+    System.out.println("Masukkan Asal Anda : "); 
+    Asal = sc.nextLine();
+    System.out.println("Masukkan Nomor HP anda : ");
+    noHP = sc.nextLine();
+    System.out.println("Masukkan PIN Anda : ");
+    PIN = sc.nextInt();
 
-        System.out.print("Masukkan nama Pendaftar yang ingin dicari: ");
-        String key = sc.nextLine();
+    // jadi nanti outputnya bakal nampilin nama nama dan identitas dari si penginput
+    //silahkan lanjutkan :)
+    
 
-        int hasil = -1;
-        for (int i = 0; i < namaPend.length; i++) {
-            if (namaPend[i].equals(key)) {
-                hasil = i + 1;
-                break;
-            }
-        }
-        if (hasil == -1) {
-            System.out.println("Pendaftar tidak ditemukan");
-        } else {
-            System.out.println("Pendaftar ada di nomor " + hasil);
-        }
     }
 }
