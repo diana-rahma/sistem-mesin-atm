1b1ee9e27fc3836d6cbf31e8cd7a00cc5c9ee630
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2023-11-01 10:35:30 +0700

    Revisi registarsi Array 2 dimesni

--- RegistrasiArray.java
+++ RegistrasiArray.java
@@ -2,25 +2,55 @@ import java.util.Scanner;
 
 public class RegistrasiArray {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        String Nama, Asal, noHP;
-        int PIN;
-
-        String [][] Identitas = new String [5][4];
-
-    System.out.println("Masukkan Nama Anda : ");
-    Nama = sc.nextLine();
-    System.out.println("Masukkan Asal Anda : "); 
-    Asal = sc.nextLine();
-    System.out.println("Masukkan Nomor HP anda : ");
-    noHP = sc.nextLine();
-    System.out.println("Masukkan PIN Anda : ");
-    PIN = sc.nextInt();
-
-    // jadi nanti outputnya bakal nampilin nama nama dan identitas dari si penginput
-    //silahkan lanjutkan :)
-    
-
+        int anggota = 5;
+        
+        String[][] identitas = new String[anggota][4];
+        
+        int jumlah = 0; 
+        
+        while (true) {
+            if (jumlah >= anggota) {
+                System.out.println("Kapasitas anggota penuh. Tidak bisa mendaftar lagi.");
+                break;
+            }
+            
+            System.out.println("Registrasi anggota ke-" + (jumlah + 1));
+            System.out.print("Nama: ");
+            String nama = input.nextLine();
+            
+            System.out.print("No. HP: ");
+            String nohp = input.nextLine();
+            
+            System.out.print("Asal: ");
+            String asal = input.nextLine();
+            
+            System.out.print("PIN: ");
+            String pin = input.nextLine();
+            
+            identitas[jumlah][0] = nama;
+            identitas[jumlah][1] = nohp;
+            identitas[jumlah][2] = asal;
+            identitas[jumlah][3] = pin;
+            
+            jumlah++; 
+            
+            System.out.print("Apakah Anda ingin mendaftar anggota lain? (y/n): ");
+            String lagi = input.nextLine();
+            if (lagi.equalsIgnoreCase("n")) {
+                break;
+            }
+        }
+        
+        System.out.println("Daftar anggota yang Terdaftar:");
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("anggota ke-" + (i + 1));
+            System.out.println("Nama: " + identitas[i][0]);
+            System.out.println("No. HP: " + identitas[i][1]);
+            System.out.println("Asal: " + identitas[i][2]);
+            System.out.println("PIN: " + identitas[i][3]);
+            System.out.println();
+        }      
     }
 }
